e248e53bb917b420120741280c58c64d25ea1bee
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-08-26 15:50:09 +0530

    added bulid

---
+++ build/precache-manifest.d1bc8c53cc22172b08930836aeefd284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26680ba35ed7b1ceb1df330bba0b9729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814308285098b44477d0",
+    "url": "/static/css/main.48d01e1f.chunk.css"
+  },
+  {
+    "revision": "27a0b576c144657fb827",
+    "url": "/static/js/2.169419f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.169419f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814308285098b44477d0",
+    "url": "/static/js/main.6ee390a9.chunk.js"
+  },
+  {
+    "revision": "6a52c016a8220507e85f",
+    "url": "/static/js/runtime-main.f1e5c95c.js"
+  }
+]);
