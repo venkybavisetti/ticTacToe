e5741b977316e37b78ca4d2ec9fd1886a5cc7cb1
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-08-28 07:57:06 +0530

    Updates

---
+++ precache-manifest.a7e02754004fba01c907bcc7dc04e249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d631f42e6e0033291476e8cb229433",
+    "url": "/ticTacToe/index.html"
+  },
+  {
+    "revision": "17a6cceb2cc15d089527",
+    "url": "/ticTacToe/static/css/main.48d01e1f.chunk.css"
+  },
+  {
+    "revision": "27a0b576c144657fb827",
+    "url": "/ticTacToe/static/js/2.169419f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToe/static/js/2.169419f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a6cceb2cc15d089527",
+    "url": "/ticTacToe/static/js/main.cddac807.chunk.js"
+  },
+  {
+    "revision": "0b016e81bbeeda728f7e",
+    "url": "/ticTacToe/static/js/runtime-main.cc7a1702.js"
+  }
+]);
